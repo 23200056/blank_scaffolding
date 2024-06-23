20947edaf86fa94e1860aa41ddeffed00c3720ad
23200056 <23200056@usc.edu.ph>
2024-06-23 23:49:31 +0800

    modified:   resources/js/components/forms/login.vue

--- resources/js/components/forms/login.vue
+++ resources/js/components/forms/login.vue
@@ -13,7 +13,7 @@
             <b-form-input id="input-2" v-model="form.password" type="password" placeholder="Enter password" required></b-form-input>
           </b-form-group>
 
-          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="submit" variant="primary">Submits</b-button>
 
         </b-form>
       </div>
